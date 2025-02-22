b739dd15b2c222e71b6a73e5a5d55e51a53e9ca9
sdecamillo <sdecamillo@fgrhs.org>
2025-02-22 12:26:57 -0500

    This is just because

--- commands/AutoCommand.py
+++ commands/AutoCommand.py
@@ -10,7 +10,7 @@ class AutoCommand(commands2.Command):
         self.driveSubsystem = driveSubsystem
         self.rollerSubsystem = rollerSubsystem
         self.timer = wpilib.Timer()
-        self.seconds = 3.0
+        self.seconds = 1.7
         self.addRequirements(self.driveSubsystem)
         self.addRequirements(self.rollerSubsystem)
         super().__init__()
@@ -20,13 +20,13 @@ class AutoCommand(commands2.Command):
         
     
     def execute(self) -> None:
-        if self.timer.get() < 0.5:
+        if self.timer.get() < 0.3:
             self.driveSubsystem.arcadeDrive(0.5, 0.0)
-        elif self.timer.get() < 1.0:
+        elif self.timer.get() < 0.6:
             self.driveSubsystem.arcadeDrive(0.0, 0.5)
-        elif self.timer.get() < 1.5:
+        elif self.timer.get() < 0.9:
             self.driveSubsystem.arcadeDrive(0.5, 0.0)
-        elif self.timer.get() < 2.0:
+        elif self.timer.get() < 1.2:
             self.driveSubsystem.arcadeDrive(0.0, 0.0)
             self.rollerSubsystem.runRoller(Constants.ROLLEY_THINGEY_EJECT_SPEED, 0.0)
 
